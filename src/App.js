04f22e1b37f22c2712dbf2986bf19649ebd5b197
Aaron Lee <45613337+Fungeey@users.noreply.github.com>
2023-07-13 13:49:17 -0400

    clean up project files

--- src/App.js
+++ src/App.js
@@ -1,15 +1,7 @@
-import './App.css';
-import { useRef} from 'react';
 import Board from './components/board';
-import UI from './components/ui';
 
 const App = () => {
-  const boardRef = useRef({});
-
-  return <div>
-      {/* <UI boardRef={boardRef}/> */}
-      <Board board={boardRef}/>
-  </div>
+  return <Board/>
 }
 
 export default App;
