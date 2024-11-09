bea3f2b944fc0aa89deb6da4a40c37dc711c23eb
Aaron Lee <45613337+Fungeey@users.noreply.github.com>
2024-11-09 10:22:49 -0500

    actually though

--- src/hooks/usescale.js
+++ src/hooks/usescale.js
@@ -10,11 +10,34 @@ const useScale = () => {
   }, [])
 
   useEffect(() => {
-    document.addEventListener('wheel', onWheel);
+
+    // element.addEventListener('wheel', event => {
+    //   const { ctrlKey } = event
+    //   if (ctrlKey) {
+    //     event.preventDefault();
+    //     return
+    //   }
+    // }, { passive: false })
+
+    document.addEventListener('wheel', onWheel, { passive: false });
     return () => document.removeEventListener('wheel', onWheel);
   }, [scale])
 
   function onWheel(e) {
+    const { ctrlKey } = e;
+    if (ctrlKey) {
+      e.preventDefault();
+
+      // scroll using mousepad pinch zoom
+      const spd = 1.1;
+      if (e.deltaY < 0)
+        setScale(util.round(Math.min(scale * spd, 5), 4));
+      else if (e.deltaY > 0)
+        setScale(util.round(Math.max(scale * (1 / spd), 0.0625), 4));
+
+      return;
+    }
+
     const spd = 1.25;
     if (e.deltaY < 0)
       setScale(util.round(Math.min(scale * spd, 5), 4));
